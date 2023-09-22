2f1cb435c5ee7625a4fc377e5551773ad93370da
nx2141 <super_red_block@yahoo.co.jp>
2023-09-22 14:56:13 +0900

    ✨ ログインサインアップ雛形できた

--- api/server.js
+++ api/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const authRoute = require("./routers/auth");
 
+
+require("dotenv").config();
 const PORT = 5000;
+
+
+app.use(express.json());
+
+app.use("/api/auth",authRoute);
+
+
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
