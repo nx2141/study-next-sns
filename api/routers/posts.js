69712cde5789252b29ce615ce54a73124fc36091
nx2141 <super_red_block@yahoo.co.jp>
2023-09-22 17:46:07 +0900

    ✨ postsまで追加

---
+++ api/routers/posts.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient();
+
+//つぶやき投稿API
+router.post("/post", async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({ message: "渡航内容がありません" });
+  }
+  try {
+    await prisma.post.create({
+      data: {
+        content,
+        authorId: 2,
+      },
+    });
+    res.status(201).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "サーバーエラーです。" });
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return res.json({ user });
+});
+
+//最新つぶやき取得用API
+//   router.post("/login", async (req, res) => {
+//     const { email, password } = req.body;
+//     const user = await prisma.user.findUnique({ where: { email } });
+//     if (!user) {
+//       return res.status(401).json({ error: "そのユーザーは存在しません。"});
+//     }
+//     const isPasswordVaild = await bcrypt.compare(password, user.password);
+
+//     if(!isPasswordVaild){
+//       return res.status(401).json({error:"そのパスワードは間違っています"});
+//     }
+
+//     const token = jwt.sign({id:user.id},process.env.SECRET_KEY,{
+//       expiresIn:"1d",//IDじゃなくて1Dだよ！
+//     });
+//     return res.json({token});
+//   });
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
+const cors = require("cors");
 
 
 require("dotenv").config();
 const PORT = 5000;
 
-
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/auth",authRoute);
-
-
+app.use("/api/posts",postsRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
